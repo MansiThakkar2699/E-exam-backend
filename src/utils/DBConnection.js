fc35baa344d526d093a179202f05a2881aa8745d
Mansi Thakkar <mansithakkar2699@gmail.com>
2026-06-11 13:01:09 +0530

    package updated

--- src/utils/DBConnection.js
+++ src/utils/DBConnection.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose")
+const dns = require("dns");
 require("dotenv").config()
+
+// Force Node to use Google DNS
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const DBConnection = () => {
     mongoose.connect(process.env.MONGO_URI).then(() => {
